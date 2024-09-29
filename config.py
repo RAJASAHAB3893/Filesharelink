38f7c980659d6dd9be882bab09197a95740d6338
RAJASAHAB3893 <Virusjones18@gmail.com>
2024-09-29 23:25:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002148944831"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from @Rajasahab3891.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from channel.")
 try:
     ADMINS=[6655939309]
     for x in (os.environ.get("ADMINS", "").split()):
